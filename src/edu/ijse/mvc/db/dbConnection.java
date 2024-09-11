a50245a97136b7981c9320bfba54767dcd86b1db
KeshakaAnjana <anjanakeshaka2@gamil.com>
2024-09-11 11:25:04 +0530

    config dbConnection

--- src/edu/ijse/mvc/db/dbConnection.java
+++ src/edu/ijse/mvc/db/dbConnection.java
@@ -4,10 +4,32 @@
  */
 package edu.ijse.mvc.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Keshaka
  */
 public class dbConnection {
+    private static dbConnection dbConnection;
     
+    private Connection Connection;
+    
+    private dbConnection()throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "ijse@1234");
+        
+    }
+    
+    public static dbConnection getInstance()throws SQLException, ClassNotFoundException{
+        if(dbConnection == null){
+            dbConnection = new dbConnection();
+        }
+        return dbConnection;
+    }
+     public Connection getConnection(){
+        return Connection;
+    }
 }
